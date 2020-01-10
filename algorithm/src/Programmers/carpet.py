99018dac48072725107a0a320a437166999ff9f1
jihunparkme <jihunpark.tech@gmail.com>
2020-01-10 11:52:52 +0900

    Add: add new file

--- algorithm/src/Programmers/carpet.py
+++ algorithm/src/Programmers/carpet.py
@@ -1,5 +1,15 @@
 def solution(brown, red):
     answer = []
-    return answer
+    area = brown + red
+    for height in range(3, area):
+        for width in range(height, brown):
+            if width * height == area:
+                if (width*2) + (height-2)*2 == brown:
+                    answer.append([width, height])
+                    break
+    return answer[0]
 
 if __name__=='__main__':
+    brown = 24
+    red = 24
+    print(solution(brown, red))
